1fd0f47ccd1ebcf0ca3917c72dc8b9d53c31f9fd
Mariapalafox2001 <43785186+Mariapalafox2001@users.noreply.github.com>
2019-04-29 11:34:31 -0400

    Add files via upload

---
+++ lesson2_problem_1.py
@@ -0,0 +1,9 @@
+classes = [' english', ' math', ' history', ' science', ' gym', ' health', ' advisory', ' production']
+for subjects in classes:
+	print('You have been enrolled in' + subjects + '.')
+
+
+
+
+
+
